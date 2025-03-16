065df14e3d5e7dbda0bfbdf676920ddea54cf00b
Tomás Rando <tomasrando@gmail.com>
2025-03-16 16:19:14 -0300

    Fix ex1

--- TP1/ex1.cpp
+++ TP1/ex1.cpp
@@ -12,20 +12,21 @@ void* tarea2(void* identificador);
 int main() {
     int N;
     int M;
-    vector<pthread_t> hilos1(N);
-    vector<pthread_t> hilos2(M);
 
-    cout << "Ingrese N";
+    cout << "Ingrese N: ";
     cin >> N;
 
-    cout << "Ingrese M";
+    cout << "Ingrese M: ";
     cin >> M;
+
+    vector<pthread_t> hilos1(N);
+    vector<pthread_t> hilos2(M);
 
     for (int i = 0; i < N; i++) {
         int r = pthread_create(&hilos1[i], NULL, tarea1, (void*) (long) i);
 
         if (r != 0) {
-            cout << "Error creando hilo";
+            cout << "Error creando hilo" << endl;
         }
     }
 
@@ -33,7 +34,7 @@ int main() {
         int r = pthread_create(&hilos2[i], NULL, tarea2, (void*) (long) i);
 
         if (r != 0) {
-            cout << "Error creando hilo";
+            cout << "Error creando hilo" << endl;
         }
     }
 
@@ -45,12 +46,12 @@ int main() {
         pthread_join(hilos2[i], NULL);
     }
 
-    cout << "Fin del programa";
+    cout << "Fin del programa" << endl;
     return 0;
 }
 
 void* tarea1(void* identificador) {
-    cout << "Thread1 instancia " << (long) identificador;
+    cout << "Thread1 instancia " << (long) identificador << endl;
     double randomTime = rand() / RAND_MAX;
     
     sleep(randomTime);
@@ -59,10 +60,10 @@ void* tarea1(void* identificador) {
 }
 
 void* tarea2(void* identificador) {
-    cout << "Thread2 instancia " << (long) identificador;
+    cout << "Thread2 instancia " << (long) identificador << endl;
     double randomTime = rand() / RAND_MAX;
     
     sleep(randomTime);
-    cout << compartida;
+    cout << compartida << endl;
     return NULL;
 }
